83095900461146dd6bbb2be67cf69bd580b9c036
fulkast <fulkast@gmail.com>
2017-04-06 19:07:50 +0200

    moving feature tests to google test

--- src/generate-AR-features.cpp
+++ src/generate-AR-features.cpp
@@ -4,7 +4,8 @@ int main(int argc, char const *argv[]) {
 
   // load object mesh and pose info
   // load camera in
-
-
+  std::string hdf5_file_name =
+    "/home/seasponge/Workspace/random-trees-with-simtrack/simtrack-data-generator/scripts/obj_file_name.h5";
+  RenderedScene myscene(hdf5_file_name);
   return 0;
 }
