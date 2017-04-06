9ed83750018670aeb1ffe1564a3d8a7d2c8efc2c
fulkast <fulkast@gmail.com>
2017-04-06 19:06:15 +0200

    adding basic scene setter getters

--- scene_rendering/include/scene_rendering/rendered_scene.h
+++ scene_rendering/include/scene_rendering/rendered_scene.h
@@ -8,4 +8,11 @@ public:
 
   RenderedScene(std::string hdf5_file_name);
 
+  void setOBJName(std::string obj_name);
+
+  std::string getOBJName() const;
+
+private:
+  std::string obj_name_;
+
 };
--- scene_rendering/src/rendered_scene.cpp
+++ scene_rendering/src/rendered_scene.cpp
@@ -1,7 +1,28 @@
 #include "scene_rendering/rendered_scene.h"
+#include <hdf5_file.h>
+
+#include <iostream>
+
 
 RenderedScene::RenderedScene(){}
 
 RenderedScene::RenderedScene(std::string hdf5_file_name) {
 
+  std::cout << "reading " << hdf5_file_name << std::endl;
+  util::HDF5File in_file(hdf5_file_name);
+  if (!in_file.checkVariableExists("obj_file_name"))
+    std::cerr << "File name doesn't exist in " << hdf5_file_name << std::endl;
+
+  std::string obj_name = in_file.readScalar<std::string>("obj_file_name");
+  std::cout << obj_name << " is the object anem" << std::endl;
+  setOBJName(obj_name);
+
+}
+
+void RenderedScene::setOBJName(std::string obj_name) {
+  obj_name_ = obj_name;
+}
+
+std::string RenderedScene::getOBJName() const {
+  return obj_name_;
 }
