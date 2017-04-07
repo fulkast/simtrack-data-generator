44d90c635c9abfcb92221b3bdf4250f153da7b98
fulkast <fulkast@gmail.com>
2017-04-07 15:19:41 +0200

    cleaning up param parsing test and test data generation

--- tests/h5_param_parsing/h5_param_parsing.cpp
+++ tests/h5_param_parsing/h5_param_parsing.cpp
@@ -6,7 +6,9 @@
 
 std::string getOBJName() {
   std::string hdf5_file_name =
-    "/home/seasponge/Workspace/random-trees-with-simtrack/simtrack-data-generator/scripts/obj_file_name.h5";
+                         "/home/seasponge/Workspace/random-trees-with-simtrack/"
+                     "simtrack-data-generator/tests/test_data/obj_file_name.h5";
+                     
   RenderedScene myscene(hdf5_file_name);
   return myscene.getOBJName();
 }
